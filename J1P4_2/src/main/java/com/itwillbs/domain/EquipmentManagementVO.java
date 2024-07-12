9f486d7f1c05f1481f9ab43c72d6b6607883e35b
hyj012 <hongmanggo7@gmail.com>
2024-07-12 13:05:56 +0900

    시설관리 vo만들기

---
+++ J1P4_2/src/main/java/com/itwillbs/domain/EquipmentManagementVO.java
@@ -0,0 +1,26 @@
+package com.itwillbs.domain;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class EquipmentManagementVO {
+	
+	//장비관리 테이블
+	
+	private int equipment_no; //장비번호 pk
+	private int manager_no; //담당자번호
+	private String equipment_name; //장비이름
+	private int cost; //비용
+	private String manufacturer; //제조사
+	private String equipment_status; //장비상태
+	private Date purchase_date; //구입날짜
+	
+	private String repair_reason; //유지보수사유
+	private String repair_approval; //유지보수승인(미승인/승인/반려)
+	private String equipment_reject; //반려사유
+	
+	
+	
+}
---
+++ J1P4_2/src/main/java/com/itwillbs/domain/FacilityVO.java
@@ -0,0 +1,18 @@
+package com.itwillbs.domain;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class FacilityVO {
+	
+	//시설테이블
+	private int facility_no; //시설번호
+	private int manager_no; //담당자 번호
+	private String facility_name; //시설이름
+	
+	private List<FacilityVO> facilityList;
+	
+	
+}
