ea1e0e9780f26174290f771d232019e197c5d70f
jiyoungyoo2 <zhzh0210@gmail.com>
2024-07-17 18:12:19 +0900

    2024-07-17 18:11 직원등록 ajax 연결 확인

--- J1P4_2/src/main/java/com/itwillbs/persistence/EmployeeDAO.java
+++ J1P4_2/src/main/java/com/itwillbs/persistence/EmployeeDAO.java
@@ -1,10 +1,15 @@
 package com.itwillbs.persistence;
 
+import java.util.List;
+
 import com.itwillbs.domain.EmployeeVO;
 
 public interface EmployeeDAO {
 	
+	// 직원 리스트 조회
+	public List<EmployeeVO> empList() throws Exception;
+	
 	// 직원등록
-	public void empJoin(EmployeeVO vo);
+	public void empJoin(EmployeeVO vo) throws Exception;
 
 }
