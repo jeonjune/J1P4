edc378c27da4867aceadfb0b873df2648e64f67c
jeonjune <jyjeon0831@naver.com>
2024-08-04 13:50:40 +0900

    에잇 똥꼬쇼하다가 충돌수정함

---
+++ J1P4_2/src/main/java/com/itwillbs/persistence/EmployeeDAOImpl.java
@@ -0,0 +1,129 @@
+package com.itwillbs.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import com.itwillbs.domain.AuthVO;
+import com.itwillbs.domain.Criteria;
+import com.itwillbs.domain.EmpAttendanceVO;
+import com.itwillbs.domain.EmployeeVO;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeDAOImpl.class);
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String NAMESPACE = "com.itwillbs.mapper.employeeMapper.";
+
+	@Override
+	public List<EmployeeVO> empList(Criteria cri) throws Exception {
+		return sqlSession.selectList(NAMESPACE+"empList");
+	}
+
+	@Override
+	public void empJoin(EmployeeVO vo) throws Exception {
+		sqlSession.insert(NAMESPACE+"empJoin", vo);
+	}
+
+	@Override
+	public void empAuth(AuthVO avo) throws Exception {
+		sqlSession.insert(NAMESPACE+"empAuth", avo);
+	}
+	
+	//아이디 중복체크
+	@Override
+	public int idCheck(String user_id) throws Exception {
+		int res = sqlSession.selectOne(NAMESPACE+"selectId", user_id);
+		logger.debug("res :"+res);
+
+		return res;
+	}
+	
+	// user_id 로 user_no 구하기
+	@Override
+	public int user_no(String user_id) throws Exception {
+		int user_no = sqlSession.selectOne(NAMESPACE+"getUN", user_id);
+		return user_no;
+	}
+
+	// 출근
+	@Override
+	public void workStart(EmpAttendanceVO vo) throws Exception {
+		sqlSession.insert(NAMESPACE+"workStart", vo);
+	}
+
+	@Override
+	public String checkWork(int user_no) throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"checkWork", user_no);
+	}
+
+	@Override
+	public void endWork(int user_no) throws Exception {
+		sqlSession.update(NAMESPACE+"endWork", user_no);
+		
+	}
+
+	@Override
+	public void outWork(int user_no) throws Exception {
+		sqlSession.update(NAMESPACE+"outWork", user_no);
+		
+	}
+
+	@Override
+	public void inWork(int user_no) throws Exception {
+		sqlSession.update(NAMESPACE+"inWork", user_no);
+		
+	}
+
+	@Override
+	public EmployeeVO empDetail(int user_no) throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"empDetail",user_no);
+	}
+	
+	// 직원 수정
+	@Override
+	public void empUpdate(EmployeeVO vo) throws Exception {
+		sqlSession.update(NAMESPACE+"empUpdate", vo);
+	}
+	// 직원 권한 수정
+	@Override
+	public void authUpdate(AuthVO vo) throws Exception {
+		sqlSession.update(NAMESPACE+"authUpdate", vo);
+		
+	}
+
+	@Override
+	public void reEmp(String user_id) throws Exception {
+		sqlSession.update(NAMESPACE+"reEmp", user_id);
+	}
+
+	@Override
+	public List<EmpAttendanceVO> monthWork(int user_no) throws Exception {
+		return sqlSession.selectList(NAMESPACE+"monthWork", user_no);
+	}
+	
+	// 페이징
+	@Override
+	public int getTotalEmpCount() throws Exception {
+		return sqlSession.selectOne(NAMESPACE+"totalEmpCount");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ J1P4_2/src/main/java/com/itwillbs/service/EmployeeServiceImpl.java
@@ -0,0 +1,130 @@
+package com.itwillbs.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.domain.AuthVO;
+import com.itwillbs.domain.Criteria;
+import com.itwillbs.domain.EmpAttendanceVO;
+import com.itwillbs.domain.EmployeeVO;
+import com.itwillbs.persistence.EmployeeDAO;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	@Inject
+	private EmployeeDAO edao;
+	
+	@Inject
+	private PasswordEncoder pwEncoder;
+
+	@Override
+	public List<EmployeeVO> empList(Criteria cri) throws Exception {
+		
+		return edao.empList(cri);
+	}
+	
+	@Override
+	public int getTotalEmpCount() throws Exception {
+		return edao.getTotalEmpCount();
+	}
+
+
+	@Override
+	public void empJoin(EmployeeVO vo) throws Exception {
+		//비밀번호 암호화
+		String pw = pwEncoder.encode(vo.getUser_pw());
+		vo.setUser_pw(pw);
+		
+		edao.empJoin(vo);
+	}
+
+	@Override
+	public void empAuth(AuthVO avo) throws Exception {
+		edao.empAuth(avo);
+	}
+	
+	//아이디 중복 체크
+	@Override
+	public int idCheck(String user_id) throws Exception {
+		
+		int res = edao.idCheck(user_id);
+
+		return res;
+		
+	}
+
+	@Override
+	public int user_no(String user_id) throws Exception {
+		int user_no = edao.user_no(user_id);
+		return user_no;
+	}
+
+	@Override
+	public void workStart(EmpAttendanceVO vo) throws Exception {
+		edao.workStart(vo);
+		
+	}
+
+	@Override
+	public String checkWork(int user_no) throws Exception {
+		return edao.checkWork(user_no);
+	}
+
+	@Override
+	public void endWork(int user_no) throws Exception {
+		edao.endWork(user_no);
+	}
+
+	@Override
+	public void outWork(int user_no) throws Exception {
+		edao.outWork(user_no);
+		
+	}
+
+	@Override
+	public void inWork(int user_no) throws Exception {
+		edao.inWork(user_no);
+		
+	}
+
+	@Override
+	public EmployeeVO empDetail(int user_no) throws Exception {
+		
+		return edao.empDetail(user_no);
+	}
+
+	@Override
+	public void empUpdate(EmployeeVO vo) throws Exception {
+		//비밀번호 암호화
+		String pw = pwEncoder.encode(vo.getUser_pw());
+		vo.setUser_pw(pw);
+		edao.empUpdate(vo);
+	}
+
+	@Override
+	public void authUpdate(AuthVO vo) throws Exception {
+		edao.authUpdate(vo);
+	}
+
+	@Override
+	public void reEmp(String user_id) throws Exception {
+		edao.reEmp(user_id);
+	}
+
+	@Override
+	public List<EmpAttendanceVO> monthWork(int user_no) throws Exception {
+		return edao.monthWork(user_no);
+	}
+	
+	
+	
+	
+	
+	
+
+}
